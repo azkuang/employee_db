087dcf2306863ea176622acacb629db9538b83e9
azkuang <alexzkuang0314@gmail.com>
2025-08-13 23:39:20 -0400

    command line stuff

---
+++ Makefile
@@ -0,0 +1,19 @@
+TARGET = bin/output
+SRC = $(wildcard src/*.c)
+OBJ = $(patsubst src/%.c, obj/%.o, $(SRC))
+
+run : clean default
+	./$(TARGET) -n test -f bin/
+
+default: $(TARGET)
+
+clean:
+	rm -f bin/*
+	rm -f obj/*.o
+	rm -f *.db
+
+$(TARGET) : $(OBJ)
+	gcc -o $@ $?
+
+obj/%.o : src/%.c
+	gcc -c $< -o $@ -Iinclude
--- src/main.c
+++ src/main.c
@@ -1,5 +1,53 @@
 #include <stdio.h>
+#include <getopt.h>
+#include <stdbool.h>
+
+// #define REQUIRE_FILE (1 << 0) // shifts 1 bit into position 0 0001
+#define REQUIRE_PATH (1 << 1) // shifts 1 bit into position 1 0010
+// #define REQUIRED (REQUIRE_FILE | REQUIRE_PATH)
 
 int main(int argc, char *argv[]) {
+	int opt = 0;
+	int require_input = 0;
+	bool new_file = false;
+	char *file_path = NULL;
+	
+	while ((opt = getopt(argc, argv, "nf:")) != -1) {
+		switch(opt) {
+			case 'n':
+				// do something with new file?
+				new_file = true;
+				break;
+			case 'f':
+				// file path?
+				file_path = optarg;
+				require_input |= REQUIRE_PATH;
+				break;
+			case ':':
+				printf("Option -%c requires an argument\n", optopt);
+				break;
+			case '?':
+				printf("Unknown option: %c\n", optopt);
+				break;
+			default:
+				printf("Unexpected input.\n");
+		}
+	}
+
+	if ((REQUIRE_PATH & require_input) != REQUIRE_PATH)
+		printf("Error: -f (path) is required.\n");
+
+//	if ((require_input & REQUIRED) != REQUIRED) {
+//		printf("Error: missing required flags.\n");
+//		if (!(require_input & REQUIRE_FILE))
+//			printf("-n (file) is required.\n");
+//		if (!(require_input & REQUIRE_PATH))
+//			printf("-f (path) is required.\n");
+//		return -1;
+//	}
+
+	printf("File name: %d\n", new_file);
+	printf("File path: %s\n", file_path);
+		
 	return 0;
 }
